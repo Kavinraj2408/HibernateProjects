18775848298373bdb0562d627ae73a1786ea8c80
Kavinraj2408 <kavinraj2408@gmail.com>
2024-02-25 20:01:31 +0530

    fetching from DB

--- DemoHibernate/src/main/java/com/kavin/DemoHibernate/App.java
+++ DemoHibernate/src/main/java/com/kavin/DemoHibernate/App.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import com.kavin.DemoHibernate.model.Alien;
 
@@ -15,19 +17,24 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Alien alien = new Alien();
-    	alien.setAid(1);
-    	alien.setAname("Kavin");
-    	alien.setColor("Green");
-    	
-    	Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
-    	
-    	SessionFactory sf = con.buildSessionFactory();
-    	
-    	Session session = sf.openSession();
-    	
-    	Transaction tx = session.beginTransaction();
-    	session.save(alien);
-    	tx.commit();
+        Alien alien = new Alien();
+//        alien.setAid(4);
+//        alien.setAname("Harsi");
+//        alien.setColor("Yellow");
+        
+        Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
+        
+        StandardServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+        
+        SessionFactory sf = con.buildSessionFactory(reg);
+        
+        Session session = sf.openSession();
+        
+        Transaction tx = session.beginTransaction();
+        
+        alien = (Alien)session.get(Alien.class, 4);
+        System.out.println(alien.toString());
+//        session.save(alien);
+        tx.commit();
     }
 }
